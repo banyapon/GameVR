ba30e4917f98785bb52cd6024961689b8dd8b777
Banyapon Poolsawas <daydevthailand@gmail.com>
2024-02-20 11:11:14 +0700

    Update Bullet.cs
    Fixed Bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float Speed = 0.7f;
+    public float SecondsUntilDestroy = 1.2f;
+    float startTime;
     void Start()
     {
-        
+        startTime = Time.time;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+    this.gameObject.transform.position += Speed *this.gameObject.transform.forward;
+        if (Time.time - startTime >= SecondsUntilDestroy)
+            {
+                Destroy(this.gameObject);
+            }
     }
+    void OnTriggerEnter(Collider collision)
+    {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                Destroy(this.gameObject);
+            }
+    }
+
 }
