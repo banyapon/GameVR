b75d33ddcd0b8af59893f73c2a28e44f902a4e33
Banyapon Poolsawas <daydevthailand@gmail.com>
2024-02-20 11:10:32 +0700

    Update Gun.cs
    Fixed Code WebXR

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WebXR;
 
 public class Gun : MonoBehaviour
 {
+    public WebXRController webXRController;
+    public float fireRate = 0.3f;
+    private float nextFire = 0.0f;
+    public GameObject bullet, gunPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        webXRController = GetComponent<WebXRController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(webXRController.GetAxis(WebXRController.AxisTypes.Trigger) > 0)
+        {
+            Fire();
+        }
+        if (webXRController.GetButton(WebXRController.ButtonTypes.ButtonA))
+        {
+            Debug.Log("Reloaded");
+        }
+    }
+    public void Fire()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(bullet, gunPosition.transform.position, gunPosition.transform.rotation);
+        }
     }
 }
